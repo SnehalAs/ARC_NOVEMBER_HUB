911079fe850850ee1770072cc00d1249149eebcd
snehal_tembhurne <stembhurne.snehal@gmail.com>
2024-11-28 16:35:37 +0530

    Pushing Project

--- src/pages/BigEnough/BigEnough.tsx
+++ src/pages/BigEnough/BigEnough.tsx
@@ -2,38 +2,40 @@ import CountUp from "react-countup";
 import "./BigEnough.css";
 
 const integrations = Array(4).fill({
-  count: 40, 
-  description: "Integrations",
+  count: 40,
+  description: "Integration",
 });
 
 const BigEnough = () => {
   return (
+
     <div className="hubnex-section">
-      <div className="header">
+       <div className="header">
         <h1>We are big enough to deliver and small enough to care</h1>
         <p>Teams large and small rely on Hubnex Labs</p>
         <button className="connect-sales-button">Connect Sales →</button>
       </div>
-
-      <div className="integration-wrapper">
-        <div className="integrations-container">
-          {integrations.map((integration, index) => (
-            <div key={index} className="integration-card">
-              <h2>
-                <CountUp
-                  start={0}
-                  end={integration.count}
-                  duration={2}
-                  suffix="+"
-                />
-              </h2>
-              <p>{integration.description}</p>
-            </div>
-          ))}
-        </div>
+    <div className="integration-wrapper">
+   
+      <div className="integration-container">
+        {integrations.map((integration, index) => (
+          <div key={index} className="integration-card">
+            <h2>
+              <CountUp
+                start={0}
+                end={integration.count}
+                duration={2}
+                suffix="+"
+              />
+            </h2>
+            <p>{integration.description}</p>
+          </div>
+        ))}
       </div>
+    </div>
     </div>
   );
 };
 
 export default BigEnough;
+
